13fc916e1cc3a34e92b0c847061658faecd6ea45
BHARGAV <BHARGAV@BHARGAV-PC>
2019-09-23 11:30:28 +0530

    creating setter and getter and adding @table,@id,and @column annotation

--- springbootproj2/src/main/java/com/springbootproj2/model/EmployeeModel.java
+++ springbootproj2/src/main/java/com/springbootproj2/model/EmployeeModel.java
@@ -1,5 +1,45 @@
 package com.springbootproj2.model;
 
-public class EmployeeModel {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "Employee")
+public class EmployeeModel {
+	@Id
+	
+	private int eid;
+	private String ename;
+	private String address;
+	private int salary;
+	public int getEid() {
+		return eid;
+	}
+	@Column(name="eid")
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	@Column(name="ename")
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getAddress() {
+		return address;
+	}
+	@Column(name="address")
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	@Column(name="salary")
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
 }
