ff737f3a9f01bdede29504a074232de809ea2147
BHARGAV <BHARGAV@BHARGAV-PC>
2019-09-23 10:40:30 +0530

    creating Employee Bean class

---
+++ springbootproj2/src/main/java/springbootproj2/Employee.java
@@ -0,0 +1,41 @@
+package springbootproj2;
+
+public class Employee {
+	private int eid;
+	private String ename;
+	private int salary;
+	private String address;
+
+	public int getEid() {
+		return eid;
+	}
+
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
