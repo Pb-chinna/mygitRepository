0e084e89eb473fed2b9e071152a0d27c822ee7c1
BHARGAV <BHARGAV@BHARGAV-PC>
2019-09-24 11:47:14 +0530

    i have created sbproj3

--- springbootproj2/src/main/java/springbootproj2/DemoApplication.java
+++ springbootproj2/src/main/java/springbootproj2/DemoApplication.java
@@ -2,11 +2,13 @@ package springbootproj2;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @RestController
 public class DemoApplication {
    @Value("${spring.application.name}")
